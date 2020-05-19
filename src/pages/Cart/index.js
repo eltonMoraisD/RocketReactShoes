01640bbed3fe60bbcb58313aa4c35cc1071bf4d0
eltonMoraisD <eltondelgadomorais@gmail.com>
2020-05-19 19:36:10 -0100

    estilizaçao do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+	return (
+		<Container>
+			<ProductTable>
+				<thead>
+					<tr>
+						<th />
+						<th>PRODUTO</th>
+						<th>QTD</th>
+						<th>SUBTOTAL</th>
+						<th />
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr>
+						<td>
+							<img
+								src="https://static.netshoes.com.br/produtos/tenis-adidas-falcon-masculino/26/COL-4399-026/COL-4399-026_zoom2.jpg?ts=1584531781&ims=326x"
+								alt="Tenis"
+							/>
+						</td>
+
+						<td>
+							<strong>Ténis muito massa</strong>
+							<span>129,90$00</span>
+						</td>
+
+						<td>
+							<div>
+								<button type="button">
+									<MdRemoveCircleOutline size={20} color="#7159c1" />
+								</button>
+
+								<input type="number" readOnly value={2} />
+
+								<button type="button">
+									<MdAddCircleOutline size={20} color="#7159c1" />
+								</button>
+							</div>
+						</td>
+
+						<td>
+							<strong>258,80$00</strong>
+						</td>
+
+						<td>
+							<button type="button">
+								<MdDelete size={20} color="#7159c1" />
+							</button>
+						</td>
+					</tr>
+				</tbody>
+			</ProductTable>
+
+			<footer>
+				<button type="button">Finalizar pedido</button>
+
+				<Total>
+					<span>TOTAL</span>
+					<strong>1990$00</strong>
+				</Total>
+			</footer>
+		</Container>
+	);
 }
