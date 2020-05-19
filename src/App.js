7fba1cfb895e232abd8568a2fb61f742a5b7017c
eltonMoraisD <eltondelgadomorais@gmail.com>
2020-05-19 17:23:16 -0100

    estilos globais

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from './styles/global';
 
 import Routes from './routes';
 
 function App() {
 	return (
 		<BrowserRouter>
+			<GlobalStyles />
 			<Routes />
 		</BrowserRouter>
 	);
